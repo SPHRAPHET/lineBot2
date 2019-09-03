4b79a1a169b7dca5e2a150f01d6e6ba9af1eaab9
SPHRAPHET <51944977+SPHRAPHET@users.noreply.github.com>
2019-09-03 14:19:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def callback():
         sendText(user,'จ้าาาา')
     elif (userText is 'บ้ายบาย') :
         sendText(user,'บ้ายบาย')
+    elif (userText is 'ไอ้บ้า') :
+        sendText(user,'เอ็งซิบ้า')
     return '',200
 
 def sendText(user, text):
